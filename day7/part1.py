4ac188c87eccaca47789d64e997bdcceef2a020e
Luke Ellis <ellisluke100@gmail.com>
2024-12-10 20:45:03 +0000

    Completed day7. Was completely overthinking it

---
+++ day7/part1.py
@@ -0,0 +1,52 @@
+import itertools
+
+def evaluate_expression(expression):
+    sum = 0
+
+    sum = int(expression[0])
+
+    for i in range(0, len(expression)-2, 2):
+        op = expression[i+1]
+        b = int(expression[i+2])
+
+        match op:
+            case '+': sum += b
+            case '*': sum *= b
+    
+    return sum
+
+problems = []
+
+with open('input.txt', 'r') as file:
+    problems = [p.rstrip('\n').split(' ') for p in file.readlines()]
+
+    for p in problems:
+        p[0] = p[0].rstrip(':')
+
+operators = ['+', '*']
+test_value_sum = 0
+
+for problem in problems:
+    test_value = int(problem[0])
+    nums = problem[1:]
+
+    operator_locations = len(nums)-1
+    operator_permutations = itertools.product(operators, repeat=operator_locations)
+
+    for perm in operator_permutations:
+        expression = [x for x in itertools.chain.from_iterable(itertools.zip_longest(nums, perm)) if x]
+
+        # Evaluate expression
+        result = evaluate_expression(expression)
+        
+        if result == test_value:
+            test_value_sum += test_value
+            break
+
+print("SUM:", test_value_sum)
+        
+
+
+
+
+
